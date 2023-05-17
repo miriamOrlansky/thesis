a885904a956d2ef02e1f96bf14b01ac5e47db7c1
miriamOrlansky <115842210+miriamOrlansky@users.noreply.github.com>
2023-05-17 10:01:22 -0400

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { Root } from "../styles/footerStyles";
 export default function Footer() {
   return (
     <Root>
-      <div>© miriam orlansky </div>
+      <div>© miriam orlanskyyyy </div>
       <div>handcrafted with love</div>
     </Root>
   );
